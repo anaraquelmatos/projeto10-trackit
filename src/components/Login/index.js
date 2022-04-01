89bf396dc560b8f51e89621730bcab6a93638e0b
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-04-01 02:14:06 -0300

    Layout da tela de login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { BallTriangle } from "react-loader-spinner";
 import styled from "styled-components";
+import logoGrande from "../../assets/img/logo-grande.png";
 
 
 function Login({ salvarToken }) {
@@ -44,38 +45,133 @@ function Login({ salvarToken }) {
     }
 
     return !loading ? (
-        <Container>
-            <form onSubmit={entrarTelaPrincipal}>
+        <Div>
+            <div className="parteSuperior">
+                <img src={logoGrande} alt="TrackIt" />
+                <h1 className="titulo">TrackIt</h1>
+            </div>
+            <Form onSubmit={entrarTelaPrincipal}>
                 <input type="email" placeholder="email" disable={habilitado} value={email} required onChange={(e) => setEmail(e.target.value)}></input>
                 <input type="password" placeholder="senha" disable={habilitado} value={senha} required onChange={(e) => setSenha(e.target.value)}></input>
                 <button disable={habilitado}>Entrar</button>
-            </form>
+            </Form>
             <Link to={`/cadastro`}>
                 <p>Não tem uma conta? Cadastre-se!</p>
             </Link>
 
-        </Container>
-    ) : <Container>
-        <ContainerLoading>
-            <Loading>
-            <BallTriangle color="#00BFFF" height={200} width={200} />
-            </Loading>
-        </ContainerLoading>
-    </Container>;
+        </Div>
+    ) :
+        <DivLoading>
+                <BallTriangle color="#00BFFF" height={120} width={120} />
+        </DivLoading>;
 
 }
 
-export default Login;
+const Div = styled.div`
+    width: 303px;
+    margin: 68px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Lexend Deca', sans-serif;
+    
+    p {
+        margin-top: 36px;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 13.976px;
+        line-height: 17px;
+        text-align: center;
+        text-decoration-line: underline;
+        color: #52B6FF;
+    }
+    
+    p:link {
+        text-decoration-line: underline;
+        color: #52B6FF;
+    }
+    p:visited {
+        text-decoration-line: underline;
+        color: #52B6FF;
+    }
+    p:active {
+        text-decoration-line: underline;
+        color: #52B6FF;
+    }
 
-const Container = styled.div`
-width: 100%;
+    .parteSuperior{
+        width: 186px;
+        margin-bottom: 33px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+
+        img{
+            width: 155px;
+            height: 90px;
+        }
+
+        .titulo{
+            font-family: 'Playball';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 68.982px;
+            line-height: 86px;
+            text-align: center;
+            color: #126BA5;
+        }
+    }
 `
-const ContainerLoading = styled.div`
-width: 100%;
-display: flex;
-justify-content: center;
-align-items: center;
+const Form = styled.form`
+        width: 303px;
+        height: 150px;
+
+    input{
+        width: 303px;
+        height: 45px;
+        margin-bottom: 6px;
+        padding-left: 11px;
+        border: 1px solid #D5D5D5;
+        box-sizing: border-box;
+        border-radius: 5px;
+        font-weight: 400;
+        font-size: 17.976px;
+        line-height: 22px;
+    }
+    input::placeholder{
+        font-style: normal;
+        font-weight: 400;
+        font-size: 19.976px;
+        line-height: 25px;
+        color: #DBDBDB;
+    }
+
+    button{
+        width: 303px;
+        height: 45px;
+        border:none;
+        background-color: #52B6FF;
+        border-radius: 4.63636px;
+        text-align: center;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20.976px;
+        line-height: 26px;
+        text-align: center;
+        color: #FFFFFF;
+    }
 `
-const Loading = styled.div`
-margin: 300px auto;
+
+const DivLoading = styled.div`
+    width: 100%;
+    heigth: 100%;
+    margin: 250px auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
 `
+
+export default Login;
