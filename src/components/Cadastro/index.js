1668cf648300f64d49c8df74b1e419b2b44860a7
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-04-01 01:44:02 -0300

    Layout da tela de cadastro

--- src/components/Cadastro/index.js
+++ src/components/Cadastro/index.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import styled from "styled-components";
+import logoGrande from "../../assets/img/logo-grande.png";
 
 
 function Cadastro() {
@@ -16,7 +18,7 @@ function Cadastro() {
     const enviarCadastro = (event) => {
 
         event.preventDefault();
-        
+
         setHabilitado(true);
 
         const URL = `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up`;
@@ -40,19 +42,121 @@ function Cadastro() {
     }
 
     return (
-        <>
-            <form onSubmit={enviarCadastro}>
+        <Div>
+            <div className="parteSuperior">
+            <img src={logoGrande} alt="TrackIt"/>
+            <h1 className="titulo">TrackIt</h1>
+            </div>
+            <Form onSubmit={enviarCadastro}>
                 <input type="email" placeholder="email" disable={habilitado} value={email} required onChange={(e) => setEmail(e.target.value)}></input>
                 <input type="password" placeholder="senha" disable={habilitado} value={senha} required onChange={(e) => setSenha(e.target.value)}></input>
                 <input type="text" placeholder="nome" disable={habilitado} value={nome} required onChange={(e) => setNome(e.target.value)}></input>
                 <input type="url" placeholder="foto" disable={habilitado} value={foto} required onChange={(e) => setFoto(e.target.value)}></input>
                 <button type="submit" disable={habilitado}>Cadastrar</button>
-            </form>
+            </Form>
             <Link to={`/`}>
                 <p>Já tem uma conta? Faça login!</p>
             </Link>
-        </>
+        </Div>
     );
 }
+
+const Div = styled.div`
+    width: 303px;
+    margin: 68px auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Lexend Deca', sans-serif;
+
+    p {
+        margin-top: 36px;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 13.976px;
+        line-height: 17px;
+        text-align: center;
+        text-decoration-line: underline;
+        color: #52B6FF;
+    }
+    
+    p:link {
+        text-decoration-line: underline;
+        color: #52B6FF;
+    }
+    p:visited {
+        text-decoration-line: underline;
+        color: #52B6FF;
+    }
+    p:active {
+        text-decoration-line: underline;
+        color: #52B6FF;
+    }
+
+    .parteSuperior{
+        width: 186px;
+        margin-bottom: 33px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+
+        img{
+            width: 155px;
+            height: 90px;
+        }
+
+        .titulo{
+            font-family: 'Playball';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 68.982px;
+            line-height: 86px;
+            text-align: center;
+            color: #126BA5;
+        }
+    }
+
+`
+const Form = styled.form`
+        width: 303px;
+        height: 249px;
+
+    input{
+        width: 303px;
+        height: 45px;
+        margin-bottom: 6px;
+        padding-left: 11px;
+        border: 1px solid #D5D5D5;
+        box-sizing: border-box;
+        border-radius: 5px;
+        font-weight: 400;
+        font-size: 17.976px;
+        line-height: 22px;
+    }
+    input::placeholder{
+        font-style: normal;
+        font-weight: 400;
+        font-size: 19.976px;
+        line-height: 25px;
+        color: #DBDBDB;
+    }
+
+    button{
+        width: 303px;
+        height: 45px;
+        border:none;
+        background-color: #52B6FF;
+        border-radius: 4.63636px;
+        text-align: center;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20.976px;
+        line-height: 26px;
+        text-align: center;
+        color: #FFFFFF;
+    }
+`
 
 export default Cadastro;
